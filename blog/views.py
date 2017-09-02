39db92a919d77f5d9b532880b8083b4ce8ab308b
danicndr <dani_val91@hotmail.com>
2017-09-02 16:59:16 -0500

    Creacion de plantillas para Posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(
+    fecha_publicacion__lte=timezone.now()).order_by('fecha_creacion')
+    return render(request, 'blog/post_list.html', {'posts': posts})
